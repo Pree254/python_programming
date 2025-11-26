30f8b2a38a486d83ced44d9ebff898bf2db5802a
pree254 <pripetym.pm@gmail.com>
2025-11-26 23:05:32 +0300

    session2

---
+++ python-data-types1/numeric-data-types-functiona.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+#numeric functions
+import math
+
+#Arithmetic Operations
+x = 4
+y = 2
+z1 = x + y
+z2 = x - y
+z3 = x / y 
+z4 = x // y
+z5 = x % y
+z6 = x * y 
+z7 = x ** y
+
+print("===Arithmetic operations===")
+print("Addition: ", z1)
+print("Subtraction: ", z2)
+print("Float Division:", z3)
+print("integer Division:", z4)
+print("modulo division:", z5)
+print("Multiplication: ", z6)
+print("Exponential:", z7)
+k = 2
+n = -2
+m = 3.567
+p = 3
+q = [1 ,2, 3, 4, 5]
+t = 49
+print ("===Advanced Computations===")
+print("Abs:", abs(2))
+print ("Abs: ", abs (-2))
+print("Ceil: ", math.ceil(m))
+print("Floor:", math.floor(m))
+print("Pow: ", pow(k,p))
+print("Max: ", max(q))
+print("Min: ", min(q))
+print("sqrt: ", math.sqrt(t))
